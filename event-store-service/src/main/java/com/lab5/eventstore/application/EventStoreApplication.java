9d0a3fa7d4820409964b053c56417df3470bbfcf
Youcef <mekki.d.youcef@outlook.fr>
2025-08-04 11:52:02 -0400

    forgot to add path

--- event-store-service/src/main/java/com/lab5/eventstore/application/EventStoreApplication.java
+++ event-store-service/src/main/java/com/lab5/eventstore/application/EventStoreApplication.java
@@ -3,7 +3,7 @@ package com.lab5.eventstore.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.lab5.eventstore"})
 public class EventStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventStoreApplication.class, args);
